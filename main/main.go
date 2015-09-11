04bdf02859a9ec8650ad2027b73b148dea1d8d18
scottcagno <scottiecagno@gmail.com>
2015-09-11 12:49:37 -0400

    first commit

---
+++ main/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/scottcagno/safemap"
+)
+
+var COUNT = 100000
+
+func ms() int64 {
+	return time.Now().UnixNano() % 1e6 / 1e3
+}
+
+func main() {
+
+	m1 := safemap.SafeMapInstance(32)
+	//m2 := make(map[string][]byte)
+
+	fmt.Printf("Adding %d elements to map...\n", COUNT)
+
+	t1 := ms()
+
+	for i := 0; i < COUNT; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		val := fmt.Sprintf("val-%d", i)
+		m1.Set(key, []byte(val))
+		//m2[key] = []byte(val)
+	}
+
+	fmt.Printf("Took %dms\n", ms()-t1)
+
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
