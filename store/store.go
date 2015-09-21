30a396ed74c54e5ed937e7b1e04cdcdfa836042e
scottcagno <scottiecagno@gmail.com>
2015-09-21 16:43:26 -0400

    modified main/main.go and store/store.go

--- store/store.go
+++ store/store.go
@@ -1,66 +1,68 @@
 package store
 
 import (
-    "sync"
+	"sync"
+
+	"github.com/cagnosolutions/safemap"
 )
 
 type SafeMapStore struct {
-    Stores map[string]*SafeMap
-    sync.RWMutex
+	SafeMaps map[string]*safemap.SafeMap
+	sync.RWMutex
 }
 
 func NewSafeMapStore(shardCount int) *SafeMapStore {
-    if shardCount == 0 || shardCount%2 != 0 {
+	if shardCount == 0 || shardCount%2 != 0 {
 		shardCount = 16
 	}
-	SHARD_COUNT = shardCount
-    return &SafeMapStore{
-        Stores: make(map[string]*SafeMap)
-    }
+	safemap.SHARD_COUNT = shardCount
+	return &SafeMapStore{
+		SafeMaps: make(map[string]*safemap.SafeMap),
+	}
 }
 
-func (sms *SafeMapStore) Set(key, fld string val interface{}) {
-    m, ok := sms.GetStore(key)
-    if !ok {
-        sms.Lock()
-        sms.Stores[storeName] = NewSafeMap(SHARD_COUNT)
-        sms.Unlock()
-    }
-    m.Set(fld, val)
+func (sms *SafeMapStore) Set(key, fld string, val interface{}) {
+	sm, ok := sms.GetSafeMap(key)
+	if !ok {
+		sms.Lock()
+		sms.SafeMaps[key] = safemap.NewSafeMap(safemap.SHARD_COUNT)
+		sms.Unlock()
+	}
+	sm.Set(fld, val)
 }
 
 func (sms *SafeMapStore) Get(key, fld string) (interface{}, bool) {
-    if m, ok := sms.GetStore(key); ok {
-        return m.Get(fld)
-    }
-    return nil, false
+	if sm, ok := sms.GetSafeMap(key); ok {
+		return sm.Get(fld)
+	}
+	return nil, false
 }
 
 func (sms *SafeMapStore) Del(key, fld string) {
-    if m, ok := sms.GetStore(key); ok {
-        m.Del(fld)
-    }
+	if sm, ok := sms.GetSafeMap(key); ok {
+		sm.Del(fld)
+	}
 }
 
 func (sms *SafeMapStore) AddStore(key string) {
-    if _, ok := sms.GetStore(key); !ok {
-        sms.Lock()
-        sms.Stores[key] = NewSafeMap(SHARD_COUNT)
-        sms.Unlock()
-    }
+	if _, ok := sms.GetSafeMap(key); !ok {
+		sms.Lock()
+		sms.SafeMaps[key] = safemap.NewSafeMap(safemap.SHARD_COUNT)
+		sms.Unlock()
+	}
 }
 
-func (sms *SafeMapStore) GetStore(key string) (*SafeMap, bool) {
-    sms.RLock()
-    m, ok := sms.Stores[key]
-    sms.RUnlock()
-    return m, ok
+func (sms *SafeMapStore) GetSafeMap(key string) (*safemap.SafeMap, bool) {
+	sms.RLock()
+	sm, ok := sms.SafeMaps[key]
+	sms.RUnlock()
+	return sm, ok
 }
 
 func (sms *SafeMapStore) DelStore(key string) {
-    if _, ok := sms.GetStore(key); ok {
-        sms.Lock()
-        delete(sms.Stores, key)
-        sms.Unlock()
-    }
+	if _, ok := sms.GetSafeMap(key); ok {
+		sms.Lock()
+		delete(sms.SafeMaps, key)
+		sms.Unlock()
+	}
 }
